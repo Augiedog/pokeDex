b82d60a7fc9318044255526080104406dd247ef2
Augie Bren <Augie>
2022-07-06 00:24:11 -0500

    calling it a night, while it still renders

--- src/Component/Gallery.js
+++ src/Component/Gallery.js
@@ -1,30 +1,30 @@
 
 function Gallery(props) {
-    const pokemon = props.pokemon.results
+    const pokemon = props.pokemon
 
-    const detailStyle = {
-        'width': '80vw',
-        'height': '20vh',
-        'border': '1px solid black',
-        'margin': '2px',
-        'backgroundImage': `url(${pokemon.sprites.front_default})`,
-        'backgroundRepeat': 'no-repeat',
-        'backgroundSize': 'cover',
-        'color': 'blue'
-    }
-    const detailView = () => {
-        return (
-            <div style={detailStyle}>
-                <h2>{pokemon.name}</h2>
-                <h3>{pokemon.height}</h3>
+    // const detailStyle = {
+    //     'width': '80vw',
+    //     'height': '20vh',
+    //     'border': '1px solid black',
+    //     'margin': '2px',
+    //     'backgroundImage': `url(${pokemon.sprites.front_default})`,
+    //     'backgroundRepeat': 'no-repeat',
+    //     'backgroundSize': 'cover',
+    //     'color': 'blue'
+    // }
+    // const detailView = () => {
+    //     return (
+    //         <div style={detailStyle}>
+    //             <h2>{pokemon.name}</h2>
+    //             <h3>{pokemon.height}</h3>
                 
-            </div>
-        )
-    }
+    //         </div>
+    //     )
+    // }
     
     return (
         <ul>
-            {pokemon.results.map(item, index => (
+            {pokemon.results.map((item, index) => (
             <li key={index}>
               {item.name} 
             </li>
