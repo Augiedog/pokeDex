d39656ad05ec021999be71363ea3b711dd933ff7
Augie Bren <Augie>
2022-07-11 22:42:08 -0500

    night

--- src/Component/InfoView.js
+++ src/Component/InfoView.js
@@ -2,6 +2,7 @@ import Card from 'react-bootstrap/Card'
 
 
 function InfoView(props) {
+    // console.log(props,"right here")
     return (
         <>
             <Card style={{ width: '95%' }}>
@@ -10,16 +11,16 @@ function InfoView(props) {
                     <Card.Title><h2>{props.pokeBall.name}  </h2></Card.Title>
                     <Card.Text>
                         {props.species.flavor_text_entries[0].flavor_text} 
-                        {props.species.flavor_text_entries[3].flavor_text}
+                        {props.species.flavor_text_entries[1].flavor_text}
                         {props.species.flavor_text_entries[4].flavor_text}
                         {props.species.flavor_text_entries[5].flavor_text}
                         {props.species.flavor_text_entries[6].flavor_text}
-                        {props.species.flavor_text_entries[7].flavor_text}
-                        {props.species.flavor_text_entries[8].flavor_text}                       
+                        {props.species.flavor_text_entries[8].flavor_text}
+                        {props.species.flavor_text_entries[9].flavor_text}                       
                     </Card.Text>
                     <Card.Text> Height={props.pokeBall.height}, Weight={props.pokeBall.weight}</Card.Text>
                     <Card.Text>
-                        Evolved from 
+                        {/* Evolved from {props.species.evolves_from_species.name || null} */}
                     </Card.Text>
                     <Card.Text>
                         Moves
