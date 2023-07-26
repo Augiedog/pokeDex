fd24e4614f23d4c61df74443fbbcf4a0873b09d0
Augie Bren <Augie>
2023-07-26 02:07:52 -0500

    working on new validation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-
+ 
 import Header from './Component/Header';
 import NavBar from './Component/NavBar'
 import Gallery from './Component/Gallery';
@@ -11,160 +11,86 @@ import Col from 'react-bootstrap/esm/Col';
 
 
 function App() {
-  const [error, setError] = useState(null)
-  const [isLoaded, setIsLoaded] = useState(false)
-  const [gotcha, setGotcha] = useState("https://pokeapi.co/api/v2/pokemon/132")
-  const [emall, setEmall] = useState("https://pokeapi.co/api/v2/pokemon-species/132")
+  const [emall, setEmall] = useState("https://pokeapi.co/api/v2/pokemon-species/132")  
+  const [ketch, setKetch] = useState("https://pokeapi.co/api/v2/pokemon/132")  
   const [twenty, setTwenty] = useState("https://pokeapi.co/api/v2/pokemon/")
   const [pokemon, setPokemon] = useState([])
   const [pokeBall, setPokeBall] = useState([])
-  const [species, setSpecies] = useState([])
-
+  const [species, setSpecies] =useState([])
+  const [error, setError] = useState(null)
+  const [tewntyIsLoad, setTwentyIsLoad] = useState(false)
+  const [emallIsLoad, setEmallIsLoad] = useState(false)
+  const [ketchIsLoad, setKetchIsLoad] = useState(false)
+  const [isLoaded, setIsLoaded] = useState(false)
+  
   // Note: the empty deps array [] means
   // this useEffect will run once
   // similar to componentDidMount()
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const twenty = await fetch(twenty)
-  //     .then(res => res.json())
-  //     .then(
-  //       (result) => {
-  //         setIsLoaded(true);
-  //         setPokemon(result);
-  //       }
-  //     ).catch(
-  //       (error) => {
-  //         setIsLoaded(true);
-  //         setError(error);
-  //       })
-  //     const gotcha = await fetch(gotcha)
-  //     .then(res => res.json())
-  //     .then(
-  //       (result) => {
-  //         setIsLoaded(true);
-  //         setPokeBall(result);
-  //         setEmall(result.species.url)
-  //       }
-  //     ).catch(
-  //       (error) => {
-  //         setIsLoaded(true);
-  //         setError(error);
-  //       })
-  //     const emall = await fetch(emall)
-  //     .then(res => res.json())
-  //     .then(
-  //       (result) => {
-  //         setIsLoaded(true);
-  //         setSpecies(result);
-  //       }
-  //     ).catch(
-  //       (error) => {
-  //         setIsLoaded(true);
-  //         setError(error);
-  //       })
-  //     const resTwenty = await twenty.json()
-  //     const resGotcha = await gotcha.json()
-  //     const resEmall = await emall.json()
-  //     setPokemon(resTwenty)
-  //     setPokeBall(resGotcha)
-  //     setEmall(resEmall)
-  //     setSpecies(emall)
-  //   }
-  //   fetchData()
-  // }, [twenty, gotcha, emall])
-
   useEffect(() => {
-    fetch(twenty)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setPokemon(result);
-        }
-      ).catch(
-        (error) => {
-          setIsLoaded(false);
-          setError(error);
-        })
+    fetch(twenty).then( async res => {
+      try {
+        const data = await res.json()
+        setTwentyIsLoad(true)
+        setPokemon(data)
+      } catch (error) {
+        setTwentyIsLoad(false)
+        setError(error)
+      }
+    })
   }, [twenty])
 
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     const response = await fetch(gotcha)
-  //       .then(res => res.json())
-  //       .then(
-  //         (result) => {
-  //           setIsLoaded(true);
-  //           setPokeBall(result);
-  //           setEmall(result.species.url)
-  //         }
-  //       ).catch(
-  //         (error) => {
-  //           setIsLoaded(true);
-  //           setError(error);
-  //         })
-  //     const resData = await response.json()
-  //     console.log("where are we", resData)
-  //   }
-  //   fetchData()
-
-  // }, [gotcha])
+  useEffect(() => {
+    fetch(ketch).then( async res => {
+      try {
+        const data = await res.json()
+        setKetchIsLoad(true)
+        setPokeBall(data)
+        setEmall(data.species.url)
+      } catch (error) {
+        setKetchIsLoad(false)
+        setError(error)
+      }
+    })     
+  }, [ketch])  
 
   useEffect(() => {
-    fetch(gotcha)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setPokeBall(result);
-          setEmall(result.species.url)
-        }
-      ).catch(
-        (error) => {
-          setIsLoaded(false);
-          setError(error);
-        })
-  }, [gotcha])  
-
-
-useEffect(() => {
-  fetch(emall)
-    .then(res => res.json())
-    .then(
-      (result) => {
-        setIsLoaded(true);
-        setSpecies(result);
+    fetch(emall).then( async res => {
+      try {
+        const data = await res.json()
+        setEmallIsLoad(true)
+        setSpecies(data)
+      } catch (error) {
+        setEmallIsLoad(false)
+        setError(error)
       }
-    ).catch(
-      (error) => {
-        setIsLoaded(false);
-        setError(error);
-      })
-}, [emall])
+    })
+  }, [emall])
 
-if (error) {
-  return <div>Error: {error.message}</div>;
-} else if (!isLoaded) {
-  return <div className='App'><h1>Loading...</h1></div>;
-} else {
-  return (
-    <Container>
-      {/* {console.log(pokemon, "first data request, APP")} */}
-      <Header />
-      <Row className="justify-content-md-center">
-        <Col>
-          <InfoView pokeBall={pokeBall} species={species} />
-        </Col>
-        <Col className='gallery'>
-          <Gallery pokemon={pokemon.results} setGotcha={setGotcha} />
-        </Col>
-      </Row>
+  if (tewntyIsLoad, emallIsLoad, ketchIsLoad === true) {
+    setIsLoaded(true)
+  }
 
-      <NavBar next={pokemon.next} previous={pokemon.previous} index={setTwenty} />
-    </Container>
-  );
-}
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div className='App'><h1>Loading...</h1></div>;
+  } else {
+    return (
+      <Container>
+        <Header />
+        <Row className="justify-content-md-center">
+          <Col>
+              <InfoView pokeBall={pokeBall} species={species} />
+          </Col>
+          <Col className='gallery'>
+            <Gallery pokemon={pokemon.results} setKetch={setKetch} />
+          </Col>
+        </Row>
+        
+        <NavBar next={pokemon.next} previous={pokemon.previous} index={setTwenty} />     
+      </Container>      
+    );
+  }
 }
 
 export default App;
